b1506b1afd430205ab78b59e288e3fa59af8c8a8
Robbie Quantum <rjhkquantum88@gmail.com>
2024-06-03 20:47:21 -0700

    initial commit

---
+++ src/main.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+bool flagger=0;
+static int cmd_blink_off(const struct shell *shell, size_t argc, char **argv)
+{
+    // Turn off LED
+    gpio_pin_set_dt(&led,0);
+    shell_print(shell, "LED blinking turned off");
+    return flagger=1;
+}
+
+SHELL_CMD_REGISTER(ledblink_off, NULL, "Turns off the LED blinking", cmd_blink_off);
+
+int main(void)
+{
+	int ret;
+	bool led_state = true;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (flagger==0) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+
+		led_state = !led_state;
+		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		k_msleep(SLEEP_TIME_MS);
+	}
+
+	return 0;
+}
